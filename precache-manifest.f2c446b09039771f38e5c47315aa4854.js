844ba57dd5845c349cf8520db8dabf864528b6ff
Saksham Singhal <singhalsaksham.mzn@gmail.com>
2020-08-06 09:29:23 +0530

    Updates

---
+++ precache-manifest.f2c446b09039771f38e5c47315aa4854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41131de2f0a93a9be740922643e852d1",
+    "url": "/recognition-catalyst/index.html"
+  },
+  {
+    "revision": "e0d2c3e2652ea19b9138",
+    "url": "/recognition-catalyst/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "283f024d30823d7ba558",
+    "url": "/recognition-catalyst/static/css/main.17047565.chunk.css"
+  },
+  {
+    "revision": "e0d2c3e2652ea19b9138",
+    "url": "/recognition-catalyst/static/js/2.bc48dc6c.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/recognition-catalyst/static/js/2.bc48dc6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "283f024d30823d7ba558",
+    "url": "/recognition-catalyst/static/js/main.42b254ea.chunk.js"
+  },
+  {
+    "revision": "c4cbdba4849e6a928008",
+    "url": "/recognition-catalyst/static/js/runtime-main.65c6d5b9.js"
+  }
+]);
